1c3b4ba9d0b82be545228288f64fefda87e9f2cb
robert-loera <robert.loera.3rd@gmail.com>
2022-05-03 15:09:17 -0500

    added pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,34 +48,19 @@ def client(session):
 
 
 @pytest.fixture
-def test_user2(client):
-    user_data = {"email": "sanjeev123@gmail.com",
-                 "password": "password123"}
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
-
-
-@pytest.fixture
 def test_user(client):
-    user_data = {"email": "sanjeev@gmail.com",
-                 "password": "password123"}
+    user_data = {"email": "example123@gmail.com",
+                 "username": "example123@gmail.com", "password": "password"}
     res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-
     new_user = res.json()
     new_user['password'] = user_data['password']
+    assert res.status_code == 201
     return new_user
 
 
 @pytest.fixture
 def token(test_user):
-    return create_access_token({"user_id": test_user['id']})
+    create_access_token({"user_id": test_user['id']})
 
 
 @pytest.fixture
@@ -86,39 +71,3 @@ def authorized_client(client, token):
     }
 
     return client
-
-
-@pytest.fixture
-def test_posts(test_user, session, test_user2):
-    posts_data = [{
-        "title": "first title",
-        "content": "first content",
-        "owner_id": test_user['id']
-    }, {
-        "title": "2nd title",
-        "content": "2nd content",
-        "owner_id": test_user['id']
-    },
-        {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user['id']
-    }, {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user2['id']
-    }]
-
-    def create_post_model(post):
-        return models.Post(**post)
-
-    post_map = map(create_post_model, posts_data)
-    posts = list(post_map)
-
-    session.add_all(posts)
-    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
-    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
-    session.commit()
-
-    posts = session.query(models.Post).all()
-    return posts
